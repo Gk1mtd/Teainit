f2583aea247260659591aab91dd6f57399cc9552
michael <michael@meinerseitz.de>
2023-06-10 18:56:07 +0200

    fix

--- screensaver_applet.py
+++ screensaver_applet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import gi
-gi.require_version('Gtk', '3.0')  # Requires Gtk version 3.0
-gi.require_version('AppIndicator3', '0.1')  # Requires AppIndicator3 version 0.1
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk, AppIndicator3
 import subprocess
 
@@ -12,11 +12,9 @@ def toggle_screensaver(widget):
     global screensaver_enabled
 
     if screensaver_enabled:
-        # Set the screensaver delay to 15 minutes
         subprocess.run(['gsettings', 'set', 'org.cinnamon.desktop.session', 'idle-delay', '900'])
         screensaver_enabled = False
     else:
-        # Set the screensaver delay to 0 (disabled)
         subprocess.run(['gsettings', 'set', 'org.cinnamon.desktop.session', 'idle-delay', '0'])
         screensaver_enabled = True
 
@@ -24,28 +22,26 @@ def build_menu():
     menu = Gtk.Menu()
 
     screensaver_item = Gtk.CheckMenuItem('Toggle Screensaver')
-    screensaver_item.connect('activate', toggle_screensaver)  # Connect toggle_screensaver function to the menu item
+    screensaver_item.connect('activate', toggle_screensaver)
     menu.append(screensaver_item)
 
     quit_item = Gtk.MenuItem('Quit')
-    quit_item.connect('activate', Gtk.main_quit)  # Connect Gtk.main_quit function to the menu item
+    quit_item.connect('activate', Gtk.main_quit)
     menu.append(quit_item)
 
     menu.show_all()
     return menu
 
 def main():
-    image_data = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAABlJREFU\nOMtjYBhggC8AAxUAj1sKIPAPQBHFAKPCuBrxgAHgTgCGYpgCUEUGAMAYEpK4Xw6MwAAAAASUVORK5C\nYII='
-
     indicator = AppIndicator3.Indicator.new(
         'screensaver-indicator',
-        '/home/michael/Repository/Teeinit/123.png',  # Path to the indicator's symbol image
+        '/home/michael/Repository/Teeinit/123.png',
         AppIndicator3.IndicatorCategory.APPLICATION_STATUS
     )
-    indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)  # Set the indicator status to active
-    indicator.set_menu(build_menu())  # Create the indicator's menu
+    indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
 
-    Gtk.main()  # Start the main Gtk event loop
+    Gtk.main()
 
 if __name__ == '__main__':
-    main()  # Call the main function if the script is executed directly
+    main()
